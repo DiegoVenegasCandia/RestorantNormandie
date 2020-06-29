bc1b29217b8af7070b51f478b4105fa85d22149d
DiegoVenegasCandia <di.venegasc@gmail.com>
2020-06-29 01:40:23 -0400

    Publicacion index mas estilo pagina restorant

---
+++ js/menu.js
@@ -0,0 +1,9 @@
+'use strict'
+var btnmenu = document.getElementById("btnmenu");
+var menu = document.getElementById("menu");
+
+btnmenu.addEventListener("click", function() {
+
+    menu.classList.toggle("mostrar");
+
+});
